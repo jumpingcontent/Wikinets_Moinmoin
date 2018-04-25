2778feca0488063916c3f50d5312e22c472cf0f2
Ron <35547141+jumpingcontent@users.noreply.github.com>
2018-04-25 18:39:26 -0500

    Create TroubleshootDocumentation.sh

---
+++ TroubleshootDocumentation.sh
@@ -0,0 +1,19 @@
+
+Here's the resulting documentation for our troubleshooting and other issues.
+
+
+--------------------------------------------
+
+
+**First issues:**
+
+1. sudo apt-get update
+
+2. sudo apt-get upgrade
+
+3. install apache2
+
+4. install UswGI
+
+
+--------------------------------------------
